ada8150184e63b596be46f484c2dd196f2cbb75a
GuanBiao <xup6ej04jp6@gmail.com>
2020-12-27 10:54:50 +0800

    the ShoppingList feature module

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [ShoppingListComponent, ShoppingEditComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'shopping-list', component: ShoppingListComponent }
+    ])
+  ]
+})
+export class ShoppingListModule {}
